de219cb7f2102ac230b3dc64866959800255105b
b2rocket <steve.leon.medina@outlook.com>
2016-01-04 14:20:26 +0000

    update

--- angmet.js
+++ angmet.js
@@ -1,21 +1,33 @@
 Tasks = new Mongo.Collection('tasks');
 
-if (Meteor.isClient) {
+  if (Meteor.isClient) {
  
+ 
+  Accounts.ui.config({
+    passwordSignupFields: "USERNAME_ONLY"
+  });
+  
+  
+  
+  
+  
   // This code only runs on the client
-  angular.module('simple-todos',['angular-meteor']);
+  angular.module('simple-todos',['angular-meteor','accounts.ui']);
  
   angular.module('simple-todos').controller('TodosListCtrl', ['$scope', '$meteor',
     function ($scope, $meteor) {
  
       $scope.tasks = $meteor.collection(function() {
         return Tasks.find($scope.getReactively('query'), {sort: {createdAt: -1}})
+        
       });
  
       $scope.addTask = function (newTask) {
         $scope.tasks.push( {
           text: newTask,
-          createdAt: new Date() }
+            createdAt: new Date(),             // current time
+            owner: Meteor.userId(),            // _id of logged in user
+            username: Meteor.user().username }  // username of logged in user
         );
         console.log("Task added" + newTask + "test");
       };
